5828105a83399d216b4375b74c2932c8a43279ab
Juan Villa <ing.villa.jimenez@gmail.com>
2025-07-03 16:35:44 -0600

    Deploy application

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-ssh -o StrictHostKeyCheking=no azureuser@135.233.113.158 <<HEREDOC
+ssh -o StrictHostKeyChecking=no azureuser@135.233.113.158 <<HEREDOC
 
   cd /home/azureuser/cf-github-actions-desarrollo-moderno-github
 
   git pull --rebase origin master
-  
+
 HEREDOC
