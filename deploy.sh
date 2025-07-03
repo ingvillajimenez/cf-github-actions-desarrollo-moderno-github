b75b9c90f5275929ec3ce3cb825dc0520706ee5a
Juan Villa <ing.villa.jimenez@gmail.com>
2025-07-03 17:20:00 -0600

    Deploy to prod server

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
+# ssh -o StrictHostKeyChecking=no azureuser@135.233.113.158 <<HEREDOC
+
+#   cd /home/azureuser/cf-github-actions-desarrollo-moderno-github
+
+#   git pull --rebase origin master
+
+# HEREDOC
+
 BRANCH=$1
 
 if [ "$BRANCH" == "master" ]; then
-  DEPLOY_PATH="/home/azureuser/cf-github-actions-desarrollo-moderno-github"
+  DEPLOY_PATH="/home/azureuser/prod"
 else
   DEPLOY_PATH="/home/azureuser/stage"
 fi
